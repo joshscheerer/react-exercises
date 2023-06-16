265aecb21f96cf12f74017368a68a7a94928e16d
Josh Scheerer <josh@armyofowls.me>
2023-06-16 08:32:58 -0400

    merge 1b

---
+++ 1.intro-to-jsx/b.intro-practice/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Owls</h1>
+    <ul>
+      <li>Horned</li>
+      <li>Barn</li>
+      <li>Snowy</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
