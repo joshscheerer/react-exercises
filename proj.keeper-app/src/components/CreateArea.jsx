ce3b4b22acdc795c770f1c16a2ccb6233bc15d85
Josh Scheerer <josh@armyofowls.me>
2023-07-14 17:24:17 -0400

    finalize Keeper App

--- proj.keeper-app/src/components/CreateArea.jsx
+++ proj.keeper-app/src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
   const [newNote, setNewNote] = useState({
     noteTitle: "",
     noteContent: "",
   });
+
+  const [expand, setExpand] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -25,21 +30,28 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
-        <input
-          onChange={handleChange}
-          name="noteTitle"
-          placeholder="Title"
-          value={newNote.noteTitle}
-        />
+      <form className="create-note">
+        {expand && (
+          <input
+            onChange={handleChange}
+            name="noteTitle"
+            placeholder="Title"
+            value={newNote.noteTitle}
+          />
+        )}
         <textarea
           onChange={handleChange}
+          onClick={() => setExpand(true)}
           name="noteContent"
           placeholder="Take a note..."
-          rows="3"
+          rows={expand ? "3" : "1"}
           value={newNote.noteContent}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={expand}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
